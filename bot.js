327abd20d3acb4bb101c559abaf9b1bfa48f1198
yeahpurn <alan.westerman@gmail.com>
2020-11-17 12:11:26 -0500

    bot.js updates/cleaning

--- bot.js
+++ bot.js
@@ -10,7 +10,10 @@ const Discord = require('discord.js');
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
+// create a security token for  the bot
 const user_token = process.env.DISCORD_TOKEN;
+
+// command parse id
 const prefix = "!";
 
 /**
@@ -21,30 +24,24 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
-// Create an event listener for messages
 client.on('message', message => {
-  // If the message is "ping"
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+	if (message.author.bot) return;
+	if (!message.content.startsWith(prefix)) return;
 
-  const commandBody = message.content.slice(prefix.length);
-  const args = commandBody.split(' ');
-  const command = args.shift().toLowerCase();
-  
-  if (command === "ping") {
-    const timeTaken = Date.now() - message.createdTimestamp;
-    message.reply('Pong! This message took ${timeTaken}ms.');
-  }
+	const commandBody = message.content.slice(prefix.length);
+	const args = commandBody.split(' ');
+	const command = args.shift().toLowerCase();
 
+	if (command === "ping") {
+		const timeTaken = Date.now() - message.createdTimestamp;
+		message.reply(`Pong! This message took ${timeTaken}ms.`);
+	}
 });
 
 
 client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
   const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
-  // Do nothing if the channel wasn't found on this server
   if (!channel) return;
-  // Send the message, mentioning the member
   channel.send(`yo sup, ${member}`);
 });
 
